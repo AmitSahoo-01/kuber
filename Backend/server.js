60a453e9c0e89da3c669c2abda21b045515f8a41
Amit Sahoo <amitkusahoo1212@gmail.com>
2026-08-22 14:26:59 +0530

    intial commit

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+
+app.get("/",(req,res)=>{
+    let sum = 0;
+    for(let i=0;i<1000000000;i++){
+        sum+=i;
+    }
+    res.send(`Hello World, ${sum}`);
+})
+
+app.get("/api",(req,res)=>{
+    res.json({message:"Hello from API"});
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
